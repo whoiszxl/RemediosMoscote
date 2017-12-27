0bc85742f047eb10f33cd0c053066744a3bd4eb7
whoiszxl <whoiszxl@gmail.com>
2017-12-27 22:56:55 +0800

    整合kafka完成

---
+++ codes/sparktrain/src/main/scala/com/whoiszxl/spark/KafkaReceiverWordCount.scala
@@ -0,0 +1,34 @@
+package com.whoiszxl.spark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * Spark Streaming 对接Kafka
+  */
+object KafkaReceiverWordCount {
+
+  def main(args: Array[String]): Unit = {
+
+    if (args.length != 4) {
+      System.err.println("params have mistakes")
+    }
+
+    var Array(zkQuorum, group, topics, numThreads) = args
+
+    var sparkConf = new SparkConf() //.setAppName("KafkaReceiverWordCount").setMaster("local[2]")
+
+    var ssc = new StreamingContext(sparkConf, Seconds(5))
+
+    val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
+
+    val messages = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap)
+
+    messages.map(_._2).flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _).print()
+
+    ssc.start()
+    ssc.awaitTermination()
+
+  }
+}
